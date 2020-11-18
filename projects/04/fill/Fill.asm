fa073dad5d2495f960ccf1fdd945370e1d451b10
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-11-18 21:53:07 +0000

    fill asm script added.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,63 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(KBDLOOP)
+  @i
+  M=0
+  @KBD
+  D=M
+  @BLACK_SCREEN_LOOP // black loop when pressed
+  D;JNE
+  @WHITE_SCREEN_LOOP // white loop when NOT pressed
+  D;JEQ
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(BLACK_SCREEN_LOOP)
+  @i
+  D=M
+  @24575
+  D=D-A
+  @END
+  D;JGT
 
-// Put your code here.
+  @i
+  D=M
+
+  @SCREEN
+  A=A+D
+  M=-1
+
+  @i
+  M=M+1
+
+  // check if key is still pressed. 
+  @KBD
+  D=M
+  @KBDLOOP 
+  D;JEQ // key no longer pressed.
+  @BLACK_SCREEN_LOOP
+  0;JMP // continue
+(END)
+
+(WHITE_SCREEN_LOOP)
+  @i
+  D=M
+  @24575
+  D=D-A
+  @END
+  D;JGT
+
+  @i
+  D=M
+
+  @SCREEN
+  A=A+D
+  M=0
+
+  @i
+  M=M+1
+
+  // check if key is still not pressed. 
+  @KBD
+  D=M
+  @KBDLOOP
+  D;JNE // key is pressed
+  @WHITE_SCREEN_LOOP
+  0;JMP
+(END)
